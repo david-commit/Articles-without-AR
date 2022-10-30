a6a9a1685f91b9bfed3797f88136a348177b9e72
david-commit <david.ondiege@students.moringaschool.com>
2022-10-30 22:02:23 +0300

    Completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,26 @@
-class Author
-  attr_accessor :name
-
+class Author 
+  attr_reader :name
+  include MagazinesMod
+  include ArticleMod
 
   def initialize(name)
     @name = name
-  
   end
 
+  def articles
+    @@all_articles.filter{|art| art.author.name == self.name}
+  end
+
+  def magazines
+    articles.map{|mag|mag.magazine}.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    magazines.map{|article| article.category}
+  end
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,29 @@
 class Magazine
-  attr_accessor :name, :category
 
+  include MagazinesMod
 
-  def initialize(name, category)
-    @name = name
-    @category = category
-
+  def self.all
+    @@all_magazines
   end
 
+  def contributors
+    ArticleMod.all.filter{ |mag| mag.magazine.name == @name}
+  end
+  
+  def find_by_name(name)
+    @@all_magazines.find{|find| find.name == self.name}
+  end
 
+  def article_titles
+    ArticleMod.all.filter{|article| article.magazine.name == @name}
+  end
+  
+  def contributing_authors
+    author_list = []
+    appearance = article_titles.map{|tit| tit.author.name}
+    appearance.tally.each{|key, value| value > 2 && author_list << key}
+    return author_list
+  end
+  
 end
+
